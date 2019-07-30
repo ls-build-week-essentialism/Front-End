1f59f795c3f33ec08c2c1b47713f337a9ac03db0
VulpineReynard <milare8@gmail.com>
2019-07-30 14:19:36 -0400

    .

--- essentialism/src/App.js
+++ essentialism/src/App.js
@@ -4,13 +4,15 @@ import 'semantic-ui-css/semantic.min.css';
 import LoginScreen from './components/LoginScreen';
 import ValueSelectorForm from './components/ValueSelectorForm';
 
+const image = "logo.jpg"
 
 
 function App() {
   return (
     <div className="App">
+      <img src={image}  alt="company-logo"/>
       <LoginScreen />
-      <ValueSelectorForm />
+      {/* <ValueSelectorForm /> */}
     </div>
   );
 }
