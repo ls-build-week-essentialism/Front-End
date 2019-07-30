038af2e8980c834656360843330bf6a1dc490d2f
yurika sakae <yurika.sun@gmail.com>
2019-07-30 11:11:13 -0700

    added router for better testing

--- essentialism/src/App.js
+++ essentialism/src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 import LoginScreen from "./components/LoginScreen";
 import ValueSelectorForm from "./components/ValueSelectorForm";
@@ -7,9 +9,11 @@ import ActivityForm from "./components/ActivityForm";
 function App() {
   return (
     <div className="App">
-      <LoginScreen />
-      <ValueSelectorForm />
-      <ActivityForm />
+      <Switch>
+        <Route exact path="/" component={LoginScreen} />
+        <Route path="/value" component={ValueSelectorForm} />
+        <Route path="/activityForm" component={ActivityForm} />
+      </Switch>
     </div>
   );
 }
--- essentialism/src/components/ActivityForm.js
+++ essentialism/src/components/ActivityForm.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Checkbox, Form } from "semantic-ui-react";
 
 function ActivityForm() {
+  const [activity, setActivity] = useState({ Activity: "jogging" });
   return (
     <Form>
       <Form.Field>
-        <label>User Input</label>
+        <label>Tell us about your activity: </label>
         <input />
       </Form.Field>
     </Form>
