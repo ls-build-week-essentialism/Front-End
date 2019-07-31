35eed479218c61379a4095708cc305f83bdea826
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-07-30 19:47:19 -0700

    User Values and default Values are pulling across to My Values; still need to exit screen with put request

--- essentialism/src/components/UserValueCard.js
+++ essentialism/src/components/UserValueCard.js
@@ -4,15 +4,14 @@ import "../../src/ValueSelectorForm.scss";
 
 //constructor function for a list of items with an "Add" button
 //onClick
-const ValueCard = (value) => (
+const UserValueCard = (value) => (
     <List celled>
         <List.Item>
             <List.Content >
               <h5>{value}</h5>      
             </List.Content>
-       
         </List.Item>
     </List>
 )
 
-export default ValueCard;
+export default UserValueCard;
--- essentialism/src/components/ValueSelectorForm.js
+++ essentialism/src/components/ValueSelectorForm.js
@@ -8,49 +8,67 @@ const defaultValues = ["Athletic Ability", "Art & Literature", "Being creative/i
 
 function ValueSelectorForm() {
   const [myValues, setMyValues] = useState([]);
+  const [userValue, setUserValue] = useState("Enter new value here")
 
+  function handleValSubmit(event) {
+    event.preventDefault();
+    console.log(event.target.value);
+    setMyValues([...myValues, userValue])
+    setUserValue("Enter new value here")
+  }
+
+  function handleDefaultSubmit(event) {
+    event.preventDefault();
+    setMyValues([...myValues, event.target.value])
+  }
 
   return (
     <div>
-
       <div className="value-selector-header">
           <h2>What is most important to you?</h2>
           <p>Select values that are important to you, or create your own below</p> 
       </div>
-
       <section className="value-selector-container">
-
         <div className="value-selector-list">
-          <Form className="create-values">
+          <Form onSubmit={handleValSubmit} className="create-values">
             <Form.Field>  
               <h3>Add Your Own Values</h3>
-              <input  />
-              <Button>Add to My Values</Button>
+              <input  
+                name="myValue"
+                onChange={event => setUserValue(event.target.value)}
+                value={userValue}
+              />
+              <Button type="submit">Add to My Values</Button>
             </Form.Field>
           </Form>
           
           <div className="default-values">
-            <h3>Choose a value</h3>                
-            {defaultValues.map(value => (
-              <div>
-                {ValueCard(value)}
-              </div>
-            ))}
+            <Form onSubmit={handleDefaultSubmit} className="default-values">
+              <h3>Choose a value</h3>                
+              {defaultValues.map(value => (
+                <div className="value-card">
+                  <div>{ValueCard(value)}</div>
+                  <div className="button-container">
+                    <Button  onClick={ () => 
+                      setMyValues([...myValues, value])
+                    }
+                    >Add to My Values</Button>
+                  </div>
+                </div>
+              ))}
+            </Form>
           </div>   
         </div> 
-      
         <div className="my-values">
           <h3>My Values</h3>
-          <List>
-            <List.Content>
-              <h5>MYVALUES1</h5>
-              <Button>Remove from My Values</Button>
-            </List.Content>
-            <List.Content>
-              <h5>MYVALUES1</h5>
-              <Button>Remove from My Values</Button>
-            </List.Content>
-          </List>
+          <div>
+            {myValues.map(userValue => (
+              <div>
+                {ValueCard(userValue)}
+              </div>
+            ))}
+          </div>
+
         </div>
       </section>
     </div>
