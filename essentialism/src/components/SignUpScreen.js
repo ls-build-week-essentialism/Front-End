0d806e5141ac2f17bbaa0debe7629951c6cf65a3
VulpineReynard <milare8@gmail.com>
2019-08-01 15:57:05 -0400

    wip: validation

--- essentialism/src/components/SignUpScreen.js
+++ essentialism/src/components/SignUpScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Form, Button, Checkbox } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
@@ -23,7 +23,14 @@ const SignUpForm = (props) => (
 );
 
 function SignUpScreen() {
+  const [formIsValid, setFormIsValid] = useState(false);
   const [user, setUser] = useState({});
+  const errors = [];
+
+  function validateForm(errors) {
+    // if (user.firstName.length > 0 && user.lastName.length > 0) {setFormIsValid(true)}
+    console.log(errors);
+  }
 
   function handleChange(event) {
     const newUser = {...user, [event.target.name]: event.target.value}
@@ -31,7 +38,11 @@ function SignUpScreen() {
     console.log(user);
   }
   function handleSubmit(event) {
-    event.preventDefault();
+    validateForm(errors);
+    setFormIsValid(true);
+    console.log(formIsValid);
+    
+    
     axios.post('https://only-essential.herokuapp.com/api/register/', user)
       .then(res => {
         console.log(user);
@@ -40,11 +51,12 @@ function SignUpScreen() {
       .catch(err => {
         console.log(err);
       })
+    if(formIsValid === true) return true;
   }
 
   return (
     <section className="sign-up-form-container">
-      <SignUpForm handleSubmit={handleSubmit} handleChange={handleChange} />
+      <SignUpForm formIsValid={formIsValid} handleSubmit={handleSubmit} handleChange={handleChange} />
     </section>
   );
 }
