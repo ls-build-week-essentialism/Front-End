0b51067192d32e08a7b1bd88f61171f38ce17f2b
yurika sakae <yurika.sun@gmail.com>
2019-08-01 11:43:03 -0700

    added styling

--- essentialism/src/components/ActivityCard.js
+++ essentialism/src/components/ActivityCard.js
@@ -1,33 +1,60 @@
 import React from "react";
-import { Button, Card } from "semantic-ui-react";
+import { Button, Card, List } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 function ActivityCard(props) {
   const { activity, deleteActivity } = props;
   console.log(props);
   return (
-    <Card>
-      <Card.Content>
-        <Card.Header>{activity.activity}</Card.Header>
-      </Card.Content>
-      <Card.Content extra>
-        <Link to={`/activity/edit/${activity.id}`}>
-          <Button basic color="blue">
-            Edit
+    <List.Item divided verticalAlign="middle">
+      <List.Content floated="right">
+        <Button.Group widths="5">
+          <Button compact size="mini" basic color="blue" attached="left">
+            <Link to={`/activity/edit/${activity.id}`}>Edit</Link>
           </Button>
-        </Link>
-        <Button
-          basic
-          color="red"
-          onClick={event => {
-            console.log("event activity", activity);
-            deleteActivity(activity);
-          }}
-        >
-          Delete
-        </Button>
-      </Card.Content>
-    </Card>
+
+          <Button
+            compact
+            attached="right"
+            size="mini"
+            basic
+            color="red"
+            onClick={event => {
+              console.log("event activity", activity);
+              deleteActivity(activity);
+            }}
+          >
+            Delete
+          </Button>
+        </Button.Group>
+      </List.Content>
+      <List.Content size="large" className="listContent-activity">
+        {activity.activity}
+      </List.Content>
+    </List.Item>
+
+    // <Card>
+    //   <Card.Content>
+    //     <Card.Header>{activity.activity}</Card.Header>
+    //   </Card.Content>
+    //   <Card.Content extra>
+    //     <Link to={`/activity/edit/${activity.id}`}>
+    //       <Button basic color="blue">
+    //         Edit
+    //       </Button>
+    //     </Link>
+    //     <Button
+    //       basic
+    //       color="red"
+    //       onClick={event => {
+    //         console.log("event activity", activity);
+    //         deleteActivity(activity);
+    //       }}
+    //     >
+    //       Delete
+    //     </Button>
+    //   </Card.Content>
+    // </Card>
   );
 }
 
