193fb3999d647033b4d8f69390c72c443b4abb9a
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-08-01 12:51:48 -0700

    Value Selector functional, needs styling

--- essentialism/src/components/ValueCard.js
+++ essentialism/src/components/ValueCard.js
@@ -2,15 +2,12 @@ import React, { useState } from "react";
 import { Button, List, Form } from "semantic-ui-react";
 import "../../src/ValueSelectorForm.scss";
 
-//constructor function for a list of items with an "Add" button
-//onClick
 const ValueCard = (value) => (
     <List celled>
         <List.Item>
             <List.Content >
               <h5>{value}</h5>      
             </List.Content>
-       
         </List.Item>
     </List>
 )
